15bca43c1d5db7db4048251e14866a637ce1c60f
Ageu Matheus <ageu_mateus@outlook.com>
2017-11-30 19:22:39 +0100

    Arquivo de configurações do projeto criado, chamando a class app e melhorado o htaccess

---
+++ src/SWPHP/System/App.php
@@ -0,0 +1,20 @@
+<?php 
+	
+	namespace SWPHP\System;
+
+	/**
+	 * 
+	 */
+
+	class App 
+	{
+		
+		/*
+		 * Roda o sistema todo
+		 */
+
+		public function rodaSistema()
+		{
+			echo "Rodando o Sistema";
+		}
+	}
